04920c2ebde4324e4f1e74a7d195695dbee62650
edisanturtle <130525378+edisanturtle@users.noreply.github.com>
2023-05-29 23:01:12 +0700

    Initial Commit

---
+++ example git/example_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  example_gitApp.swift
+//  example git
+//
+//  Created by edi santoso on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct example_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
